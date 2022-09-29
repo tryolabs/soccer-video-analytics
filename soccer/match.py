2f12e79018e3e1f02983df1ecd947a7d3f688af8
Diego Fernandez <diefernandez89@gmail.com>
2022-09-29 08:55:25 -0300

    feat: team passes counter

--- soccer/match.py
+++ soccer/match.py
@@ -102,8 +102,7 @@ class Match:
                 self.last_player_with_ball = self.init_player_with_ball
 
             last_player_has_id = (
-                self.last_player_with_ball
-                and "id" in self.last_player_with_ball.detection.data
+                self.last_player_with_ball and "id" in self.last_player_with_ball.detection.data
             )
             closest_player_has_id = (
                 self.closest_player and "id" in self.closest_player.detection.data
@@ -117,22 +116,19 @@ class Match:
 
             if different_player and same_team:
                 # Generate new pass
-                start_pass = self.last_player_with_ball.closest_foot_to_ball_abs(
-                    self.ball
-                )
+                start_pass = self.last_player_with_ball.closest_foot_to_ball_abs(self.ball)
                 start_pass_bbox = [start_pass, start_pass]
+
+                team: Team = closest_player.team
 
                 new_pass = Pass(
                     start_ball_bbox=start_pass_bbox,
                     end_ball_bbox=ball.detection.absolute_points,
-                    color=closest_player.team.color,
+                    team=team,
                 )
-                self.passes.append(new_pass)
+                team.passes.append(new_pass)
             else:
-                if (
-                    self.player_with_ball_counter
-                    < self.player_with_ball_threshold_dif_team
-                ):
+                if self.player_with_ball_counter < self.player_with_ball_threshold_dif_team:
                     return
 
             self.last_player_with_ball = self.closest_player
@@ -160,26 +156,19 @@ class Match:
         self.duration += 1
 
     def update_pass(self):
+        """
+        Updates the player with the ball counter
+        """
         init_player = self.init_player_with_ball
         closest_player = self.closest_player
 
         init_player_has_id = init_player and "id" in init_player.detection.data
         closest_player_has_id = closest_player and "id" in closest_player.detection.data
 
-        same_id = (
-            init_player_has_id
-            and closest_player_has_id
-            and (
-                init_player.detection.data["id"] == closest_player.detection.data["id"]
-            )
-        )
+        same_id = init_player_has_id and closest_player_has_id and (init_player == closest_player)
 
         different_id = (
-            init_player_has_id
-            and closest_player_has_id
-            and (
-                init_player.detection.data["id"] != closest_player.detection.data["id"]
-            )
+            init_player_has_id and closest_player_has_id and not (init_player == closest_player)
         )
 
         if same_id:
@@ -204,20 +193,20 @@ class Match:
     def time_possessions(self) -> str:
         return f"{self.home.name}: {self.home.get_time_possession(self.fps)} | {self.away.name}: {self.away.get_time_possession(self.fps)}"
 
-    def possession_bar(self, frame: np.ndarray, origin: tuple) -> np.ndarray:
+    def possession_bar(self, frame: PIL.Image.Image, origin: tuple) -> PIL.Image.Image:
         """
         Draw possession bar
 
         Parameters
         ----------
-        frame : np.ndarray
+        frame : PIL.Image.Image
             Frame
         origin : tuple
             Origin (x, y)
 
         Returns
         -------
-        np.ndarray
+        PIL.Image.Image
             Frame with possession bar
         """
 
@@ -250,6 +239,74 @@ class Match:
 
         # Draw first one rectangle or another in orther to make the
         # rectangle bigger for better rounded corners
+        frame = self.draw_counter_rectangle(
+            frame=frame,
+            ratio=ratio,
+            left_rectangle=left_rectangle,
+            left_color=left_color,
+            right_rectangle=right_rectangle,
+            right_color=right_color,
+        )
+
+        # Draw home text
+        if ratio > 0.15:
+            home_text = f"{int(self.home.get_percentage_possession(self.duration) * 100)}%"
+
+            frame = Draw.text_in_middle_rectangle(
+                img=frame,
+                origin=left_rectangle[0],
+                width=left_rectangle[1][0] - left_rectangle[0][0],
+                height=left_rectangle[1][1] - left_rectangle[0][1],
+                text=home_text,
+                color=self.home.text_color,
+            )
+
+        # Draw away text
+        if ratio < 0.85:
+            away_text = f"{int(self.away.get_percentage_possession(self.duration) * 100)}%"
+
+            frame = Draw.text_in_middle_rectangle(
+                img=frame,
+                origin=right_rectangle[0],
+                width=right_rectangle[1][0] - right_rectangle[0][0],
+                height=right_rectangle[1][1] - right_rectangle[0][1],
+                text=away_text,
+                color=self.away.text_color,
+            )
+
+        return frame
+
+    def draw_counter_rectangle(
+        self,
+        frame: PIL.Image.Image,
+        ratio: float,
+        left_rectangle: tuple,
+        left_color: tuple,
+        right_rectangle: tuple,
+        right_color: tuple,
+    ) -> PIL.Image.Image:
+        """Draw counter rectangle for both teams
+
+        Parameters
+        ----------
+        frame : PIL.Image.Image
+            Video frame
+        ratio : float
+            counter proportion
+        left_rectangle : tuple
+            rectangle for the left team in counter
+        left_color : tuple
+            color for the left team in counter
+        right_rectangle : tuple
+            rectangle for the right team in counter
+        right_color : tuple
+            color for the right team in counter
+
+        Returns
+        -------
+        PIL.Image.Image
+            Drawed video frame
+        """
         if ratio < 0.15:
             left_rectangle[1][0] += 20
 
@@ -285,11 +342,79 @@ class Match:
                 radius=15,
             )
 
+        return frame
+
+    def passes_bar(self, frame: PIL.Image.Image, origin: tuple) -> PIL.Image.Image:
+        """
+        Draw passes bar
+
+        Parameters
+        ----------
+        frame : PIL.Image.Image
+            Frame
+        origin : tuple
+            Origin (x, y)
+
+        Returns
+        -------
+        PIL.Image.Image
+            Frame with passes bar
+        """
+
+        bar_x = origin[0]
+        bar_y = origin[1]
+        bar_height = 29
+        bar_width = 310
+
+        home_passes = len(self.home.passes)
+        away_passes = len(self.away.passes)
+        total_passes = home_passes + away_passes
+
+        try:
+            home_ratio = home_passes / total_passes
+        except Exception:
+            home_ratio = 0
+        try:
+            away_ratio = away_passes / total_passes
+        except Exception:
+            away_ratio = 0
+
+        ratio = home_ratio
+
+        # Protect against too small rectangles
+        if ratio < 0.07:
+            ratio = 0.07
+
+        if ratio > 0.93:
+            ratio = 0.93
+
+        left_rectangle = (
+            origin,
+            [int(bar_x + ratio * bar_width), int(bar_y + bar_height)],
+        )
+
+        right_rectangle = (
+            [int(bar_x + ratio * bar_width), bar_y],
+            [int(bar_x + bar_width), int(bar_y + bar_height)],
+        )
+
+        left_color = self.home.board_color
+        right_color = self.away.board_color
+
+        # Draw first one rectangle or another in orther to make the
+        # rectangle bigger for better rounded corners
+        frame = self.draw_counter_rectangle(
+            frame=frame,
+            ratio=ratio,
+            left_rectangle=left_rectangle,
+            left_color=left_color,
+            right_rectangle=right_rectangle,
+            right_color=right_color,
+        )
+
         # Draw home text
         if ratio > 0.15:
-            home_text = (
-                f"{int(self.home.get_percentage_possession(self.duration) * 100)}%"
-            )
+            home_text = f"{int(home_ratio * 100)}%"
 
             frame = Draw.text_in_middle_rectangle(
                 img=frame,
@@ -302,9 +427,8 @@ class Match:
 
         # Draw away text
         if ratio < 0.85:
-            away_text = (
-                f"{int(self.away.get_percentage_possession(self.duration) * 100)}%"
-            )
+            away_text = f"{int(away_ratio * 100)}%"
+
             frame = Draw.text_in_middle_rectangle(
                 img=frame,
                 origin=right_rectangle[0],
@@ -455,7 +579,39 @@ class Match:
 
         return frame
 
-    def draw(
+    def draw_debug(self, frame: PIL.Image.Image) -> PIL.Image.Image:
+        """Draw line from closest player feet to ball
+
+        Parameters
+        ----------
+        frame : PIL.Image.Image
+            Video frame
+
+        Returns
+        -------
+        PIL.Image.Image
+            Drawed video frame
+        """
+        if self.closest_player and self.ball:
+            closest_foot = self.closest_player.closest_foot_to_ball(self.ball)
+
+            color = (0, 0, 0)
+            # Change line color if its greater than threshold
+            distance = self.closest_player.distance_to_ball(self.ball)
+            if distance > self.ball_distance_threshold:
+                color = (255, 255, 255)
+
+            draw = PIL.ImageDraw.Draw(frame)
+            draw.line(
+                [
+                    tuple(closest_foot),
+                    tuple(self.ball.center),
+                ],
+                fill=color,
+                width=2,
+            )
+
+    def draw_posession_counter(
         self,
         frame: PIL.Image.Image,
         counter_background: PIL.Image.Image,
@@ -463,7 +619,7 @@ class Match:
     ) -> PIL.Image.Image:
         """
 
-        Draw elements of the match in frame
+        Draw elements of the posession in frame
 
         Parameters
         ----------
@@ -510,32 +666,77 @@ class Match:
             height=31,
             width=150,
         )
-        frame = self.possession_bar(
-            frame, origin=(counter_origin[0] + 35, counter_origin[1] + 195)
-        )
+        frame = self.possession_bar(frame, origin=(counter_origin[0] + 35, counter_origin[1] + 195))
 
         if self.closest_player:
             frame = self.closest_player.draw_pointer(frame)
 
         if debug:
-            # Draw line from closest player feet to ball
-            if self.closest_player and self.ball:
-                closest_foot = self.closest_player.closest_foot_to_ball(self.ball)
+            frame = self.draw_debug(frame=frame)
 
-                color = (0, 0, 0)
-                # Change line color if its greater than threshold
-                distance = self.closest_player.distance_to_ball(self.ball)
-                if distance > self.ball_distance_threshold:
-                    color = (255, 255, 255)
+        return frame
 
-                draw = PIL.ImageDraw.Draw(frame)
-                draw.line(
-                    [
-                        tuple(closest_foot),
-                        tuple(self.ball.center),
-                    ],
-                    fill=color,
-                    width=2,
-                )
+    def draw_passes_counter(
+        self,
+        frame: PIL.Image.Image,
+        counter_background: PIL.Image.Image,
+        debug: bool = False,
+    ) -> PIL.Image.Image:
+        """
+
+        Draw elements of the passes in frame
+
+        Parameters
+        ----------
+        frame : PIL.Image.Image
+            Frame
+        counter_background : PIL.Image.Image
+            Counter background
+        debug : bool, optional
+            Whether to draw extra debug information, by default False
+
+        Returns
+        -------
+        PIL.Image.Image
+            Frame with elements of the match
+        """
+
+        # get width of PIL.Image
+        frame_width = frame.size[0]
+        counter_origin = (frame_width - 540, 40)
+
+        frame = self.draw_counter_background(
+            frame,
+            origin=counter_origin,
+            counter_background=counter_background,
+        )
+
+        frame = self.draw_counter(
+            frame,
+            origin=(counter_origin[0] + 35, counter_origin[1] + 130),
+            text=self.home.abbreviation,
+            counter_text=str(len(self.home.passes)),
+            color=self.home.board_color,
+            text_color=self.home.text_color,
+            height=31,
+            width=150,
+        )
+        frame = self.draw_counter(
+            frame,
+            origin=(counter_origin[0] + 35 + 150 + 10, counter_origin[1] + 130),
+            text=self.away.abbreviation,
+            counter_text=str(len(self.away.passes)),
+            color=self.away.board_color,
+            text_color=self.away.text_color,
+            height=31,
+            width=150,
+        )
+        frame = self.passes_bar(frame, origin=(counter_origin[0] + 35, counter_origin[1] + 195))
+
+        if self.closest_player:
+            frame = self.closest_player.draw_pointer(frame)
+
+        if debug:
+            frame = self.draw_debug(frame=frame)
 
         return frame
